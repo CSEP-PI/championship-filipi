34f8546ee3f9f70d5d07bcd935b1c5d2662b7b07
luiz filipi <luizfilipi3246@gmail.com>
2025-06-06 11:57:29 -0300

    ajustes e inicio TimeDetail

--- championship/src/pages/TimeDetail/index.js
+++ championship/src/pages/TimeDetail/index.js
@@ -1,8 +1,40 @@
 import gigante from '../../assets/gigante.png'
+import './style.css'
 
 function TimeDetail(){
     return(
-        <div>
+        <div className='TimeDetailContainer'>
+            <div>
+                <img src={gigante} alt='Time' />
+                <h1>TimeA</h1>
+                <h2>Capitão</h2>
+            </div>
+
+            <div>
+                <div>
+                    <h3>Camps</h3>
+                </div>
+                <div>
+                    <p>e2025</p>
+                    <p>e2024</p>
+                    <p>e2023</p>
+                </div>
+            </div>
+
+            <div>
+                <ul>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                    <li>Jogador</li>
+                </ul>
+            </div>
         </div>
     )
 }
